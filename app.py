df46ed69018333cb2f1fb468f0b06cd8d37aea6c
Nakul-Nimbekar <nakulnimbekar2003@gmail.com>
2025-06-11 18:23:31 +0530

    app.py Updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
@@ -19,4 +20,7 @@ def contact():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
