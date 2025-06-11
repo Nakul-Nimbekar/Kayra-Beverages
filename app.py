f84598f3b4fd38aa8a7947c44f5ba013c33cd005
Nakul-Nimbekar <nakulnimbekar2003@gmail.com>
2025-06-11 16:51:21 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, flash
+
+app = Flask(__name__)
+app.secret_key = 'kyara_secret'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    print(f"New message from {name} ({email}): {message}")
+
+    flash('Thanks for contacting us! We’ll get back to you soon.')
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
